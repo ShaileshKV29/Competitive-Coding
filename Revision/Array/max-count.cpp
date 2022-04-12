64aa5a15a7245b7ce07f5282e1d2446de76a4f8c
ShaileshKV29 <shailesh29062001@gmail.com>
2022-04-12 22:32:10 +0530

    Some Changes

--- Revision/Array/max-count.cpp
+++ Revision/Array/max-count.cpp
@@ -6,6 +6,7 @@ int main()
     int arr[] = {1,12,34,4234,1,12,33,34};
     int max = INT32_MIN;
     int count = 0;
+    int item;
     for(int i = 0; i < 8; i++)
     {
         for(int j = i+1; j < 8; j++)
@@ -15,10 +16,15 @@ int main()
         }
         
         if(count > max)
+        {
             max = count;
+            item = arr[i];
+        }
+        count = 0;
     }
 
-    cout << max;
+    cout << item << endl;
+    cout << max << endl;
 
     return 0;
 }
