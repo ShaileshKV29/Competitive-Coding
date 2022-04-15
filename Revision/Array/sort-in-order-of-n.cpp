de135d336861da36c9696c0f7e886a36dd28840a
ShaileshKV29 <shailesh29062001@gmail.com>
2022-04-15 23:15:25 +0530

    Sort in O(n)

--- Revision/Array/sort-in-order-of-n.cpp
+++ Revision/Array/sort-in-order-of-n.cpp
@@ -4,7 +4,38 @@ using namespace std;
 int main()
 {
     int arr[] = {1,2,23,4,5,67,87,89,90,9,0,8,65};
-    int max;
+    int max = INT32_MIN;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0; i < size; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    cout << max << endl;
+    int hmap[max + 1] = {0};
+    for(int i = 0; i < size; i++)
+    {
+        hmap[arr[i]]++;
+    }
+
+    for(int i = 0, j = 0; i < max + 1; i++)
+    {
+        // cout << hmap[i] << " ";
+        if(hmap[i] > 0)
+        {
+            arr[j++] = i;
+        }
+    }
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+        // if(hmap[i] > 0)
+        // {
+        //     arr[j] = i;
+        // }
+    }
 
     return 0;
 }
