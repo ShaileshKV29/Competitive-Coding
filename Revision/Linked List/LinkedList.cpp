a1e6510d62f8c4eca4a6ff7a350e7ec02dd423a0
ShaileshKV29 <shailesh29062001@gmail.com>
2022-04-20 22:19:39 +0530

    LinkedList Class

--- Revision/Linked List/LinkedList.cpp
+++ Revision/Linked List/LinkedList.cpp
@@ -10,7 +10,23 @@ class Node
 
 class LinkedList
 {
+    int size;
+    Node *first;
 
+    public:
+        LinkedList(int arr[], int n)
+        {
+            Node *p = new Node;
+            p->data = arr[0];
+            first = p;
+
+            for(int i = 1; i < n; i++)
+            {
+                p = new Node;
+                p->data = arr[i];
+                p->next = nullptr;
+            }
+        }
 };
 
 int main()
