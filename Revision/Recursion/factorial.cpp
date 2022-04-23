c6f221ffb1e0b85f63d59f2da93cd83e4fcf9f15
ShaileshKV29 <shailesh29062001@gmail.com>
2022-04-23 22:24:45 +0530

    Factorial

---
+++ Revision/Recursion/factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if(n > 0)
+        return factorial(n - 1)*n;
+    
+    return 1;
+}
+
+int main()
+{
+    cout << factorial(5);
+
+    return 0;
+}
