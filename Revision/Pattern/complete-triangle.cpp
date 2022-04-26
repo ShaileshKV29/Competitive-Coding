eb937e4fa6b6626965c7d8d3ce48d8b10c5b5cb7
ShaileshKV29 <shailesh29062001@gmail.com>
2022-04-26 23:39:19 +0530

    Complete Triangle with 3 Loops

--- Revision/Pattern/complete-triangle.cpp
+++ Revision/Pattern/complete-triangle.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 int main()
 {
-    
+    for(int i = 1; i <= 10; i++)
+    {
+        for(int j = 1; j <= 10; j++)
+        {
+            if(i + j > 10)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+
+        for(int k = 2; k <= i; k++)
+            cout << "* ";
+        cout << endl;
+    }
 
     return 0;
 }
