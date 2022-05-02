f0858042c73c2b2ecc8d88a86d8fda497a747824
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-02 22:20:03 +0530

    First and Last Occuracne

--- Binary Search/first-and-last-occurance-2.cpp
+++ Binary Search/first-and-last-occurance-2.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int binary_search(int arr[], int n, int key)
+int binary_search_first(int arr[], int n, int key)
 {
     int l = 0;
     int h = n-1;
@@ -24,10 +24,34 @@ int binary_search(int arr[], int n, int key)
     return first;
 }
 
+int binary_search_last(int arr[], int n, int key)
+{
+    int l = 0;
+    int h = n-1;
+    int mid;
+    int first = - 1;
+    while(l <= h)
+    {
+        mid = (l+h)/2;
+        if(key == arr[mid])
+        {
+            first = mid;
+            l = mid + 1;
+        }
+        else if(key < arr[mid])
+            h = mid - 1;
+        else if(key > arr[mid])
+            l = mid + 1;
+    }
+
+    return first;
+}
+
 int main()
 {
     int arr[10] = {1,1,2,2,2,2,2,2,4,5}; 
-    cout << binary_search(arr, 10, 4);   
+    cout << binary_search_first(arr, 10, 2) << endl;   
+    cout << binary_search_last(arr, 10, 2) << endl;   
 
     return 0;
 }
