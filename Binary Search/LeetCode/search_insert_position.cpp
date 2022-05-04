2f357a40b1ddc353aee2af7937f2232aa02da5cb
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-04 13:03:54 +0530

    Search Insert Position

---
+++ Binary Search/LeetCode/search_insert_position.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int searchInsert(vector<int>& arr, int target)
+{
+    int l = 0;
+    int h = arr.size() - 1;
+    int mid;
+    while(l <= h)
+    {
+        mid = (l+h)/2;
+
+        if(arr[mid] == target)
+            return mid;
+        else if(target < arr[mid])
+            h = mid - 1;
+        else if(target > arr[mid])
+            l = mid + 1;
+    }
+    
+    if(target < arr[mid])
+        return mid;
+    return mid + 1;
+}
+
+int main()
+{
+    vector<int> arr = {1,3,5,6,8};
+    cout << searchInsert(arr, 4);   
+
+    return 0;
+}
