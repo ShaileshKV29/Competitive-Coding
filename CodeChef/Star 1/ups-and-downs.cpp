71c507a0c3d8a83f3d085b4f4fc8d6f04454ce54
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-12 01:48:02 +0530

    Upds and Down

---
+++ CodeChef/Star 1/ups-and-downs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+        
+        sort(arr, arr + n);
+        for(int i = 1; i < n - 1; i+=2)
+        {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+        
+        for(int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+	return 0;
+}
