c88f95e98569b081c5fbf84140832b66fe88ed64
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-11 23:07:03 +0530

    Fibonacci

--- Revision/Recursion/fibonacii.cpp
+++ Revision/Recursion/fibonacii.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int n)
+{
+    if(n > 2)
+        return fibonacci(n-1) + fibonacci(n-2);
+    else
+        return 1;
+
+    return 0;
+}
+
 int main()
 {
-    cout << "Someone give me some hope!" << endl;
-    cout << "Someone give me some hope! Please!";
+    cout << fibonacci(10);
 
     return 0;
 }
