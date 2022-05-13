01b251da1d71b318107d588e21e2051c8a3f8af8
ShaileshKV29 <shailesh29062001@gmail.com>
2022-05-13 23:53:23 +0530

    Array Merge

---
+++ Revision/Array/array-merge.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[] = {1,3,4,5,6,8,10,12};
+    int b[] = {0,2,3,3,7,8,9,11};
+    int i = 0, j = 0;
+    int size_a = sizeof(a)/sizeof(a[0]);
+    int size_b = sizeof(b)/sizeof(b[0]);
+    while(i != size_a || j != size_b)
+    {
+        
+    }
+
+    return 0;
+}
