eae2309119fbb206c2a6a43cec2936318a017a37
ShaileshKV29 <shailesh29062001@gmail.com>
2022-07-07 23:45:52 +0530

    Max Area Histogram

---
+++ Stack/max_area_histogram_stack.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> nearestSmallerLeft(int arr[], int n)
+{
+    
+}
+
+int maxAreaHist(int arr[], int n)
+{
+
+}
+
+int main()
+{
+    
+
+    return 0;
+}
