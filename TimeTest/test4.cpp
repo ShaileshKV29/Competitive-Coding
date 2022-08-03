59d7ed00a25566cc98b6cc7542b0dc71d27702b5
ShaileshKV29 <shailesh29062001@gmail.com>
2022-08-03 23:13:51 +0530

    Test 3 and Test 4

---
+++ TimeTest/test4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i < n/2; i++)
+    {
+        if(i*i*i == n)
+        {
+            cout << "Yes";
+            break;
+        }
+    }
+
+
+    return 0;
+}
