e72d223bfb6f0a1a4f516d4c86f90c57be71729b
ShaileshKV29 <shailesh29062001@gmail.com>
2022-09-22 23:34:25 +0530

    Remove Loop

---
+++ LinkedList/GFG/remove-loop.cpp
@@ -0,0 +1,165 @@
+//{ Driver Code Starts
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+
+// } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+#include<map>
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    void removeLoop(Node* head)
+    {
+        // code here
+        // just remove the loop without losing any nodes
+        if(head == nullptr)
+            return;
+        if(head->next == nullptr)
+            return;
+            
+        Node *p = detectNode(head);
+        if(p == nullptr)
+            return;
+        Node *q = nullptr;
+        Node *k = head;
+        while(k != p)
+        {
+            p = p->next;
+            k = k->next;
+        }
+        
+        q = p;
+        while(q->next != p)
+            q = q->next;
+        if(q != nullptr)
+            q->next = nullptr;
+    }
+    
+    Node* detectNode(Node* head)
+    {
+        Node *p = head;
+        Node *q = head;
+        if(head == nullptr)
+            return nullptr;
+        if(head->next == nullptr)
+            return nullptr;
+        while(p != nullptr && q != nullptr && q->next != nullptr)
+        {
+            p = p->next;
+            q = q->next->next;
+            if(p == q)
+                return q;
+        }
+        
+        return nullptr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        ob.removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
