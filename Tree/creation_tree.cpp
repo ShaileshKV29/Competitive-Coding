f7875167e5d6b53f257539ef48a6867db73608a0
ShaileshKV29 <shailesh29062001@gmail.com>
2022-10-09 01:05:47 +0530

    Tree Creation

--- Tree/creation_tree.cpp
+++ Tree/creation_tree.cpp
@@ -1,8 +1,77 @@
 #include<iostream>
+#include<queue>
 using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+
+        Node(int d)
+        {
+            this->data = d;
+            this->left = nullptr;
+            this->right = nullptr;
+        }
+};
+
+class Tree
+{
+    Node *root = nullptr;
+
+    public:
+    void createTree()
+    {
+        int root_data;
+        cout << "Enter root data: ";
+        cin >> root_data;
+
+        if(root_data == -1)
+            return;
+        else
+            root = new Node(root_data);
+        
+        queue<Node *> que;
+        que.push(root);
+
+        while(!que.empty())
+        {
+            int left;
+            int right;
+            Node *p = que.front();
+            que.pop();
+            cout << endl;
+            cout << "Node is: " << p->data << endl;
+
+            cout << "Enter Left Child: ";
+            cin >> left;
+
+
+            if(left != -1)
+            {
+                Node *t = new Node(left);
+                p->left = t;
+                que.push(t);
+            }
+
+            cout << "Enter Right Child: ";
+            cin >> right;
+
+            if(right != -1)
+            {
+                Node *t = new Node(right);
+                p->right = t;
+                que.push(t);
+            }
+        }
+    }
+};
 
 int main()
 {
-
+    Tree tree;
+    tree.createTree();
     return 0;
 }
