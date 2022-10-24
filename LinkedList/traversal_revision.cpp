4c0b1124974f0f4087ae0cc59cbce83c4a2201e4
ShaileshKV29 <shailesh29062001@gmail.com>
2022-10-24 23:41:01 +0530

    Traversal Revision

---
+++ LinkedList/traversal_revision.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList
+{
+
+};
+
+int main()
+{
+    
+
+    return 0;
+}
