b8dc5eccb33675781e1b697072d5189350b37fc2
ShaileshKV29 <shailesh29062001@gmail.com>
2022-11-06 23:50:48 +0530

    PostOrder

--- Tree/PostOrder.cpp
+++ Tree/PostOrder.cpp
@@ -1,7 +1,110 @@
 #include<iostream>
+#include <queue>
+#include <stack>
 using namespace std;
+
+class Node
+{
+    public: 
+        int data;
+        Node *left;
+        Node *right;
+
+        Node(int d)
+        {
+            this->data = d;
+            left = nullptr;
+            right = nullptr;
+        }
+};
+
+class Tree
+{
+    Node *root = nullptr;
+
+    public:
+        Tree(int arr[], int n)
+        {
+            root = new Node(arr[0]);
+
+            queue<Node *> que;
+            que.push(root);
+
+            int i = 1;
+            while(i < n)
+            {
+                Node *p = que.front();
+                que.pop();
+
+                if(i < n){
+                    Node *t = new Node(arr[i]);
+                    que.push(t);
+                    p->left = t;
+                    i++;
+                }
+
+                if(i < n)
+                {
+                    Node *t = new Node(arr[i]);
+                    que.push(t);
+                    p->right = t;
+                    i++;
+                }
+            }
+        }
+
+        // void preOrder()
+        // {
+        //     preOrderRev(root);
+        // }
+
+        // void preOrderRev(Node *p)
+        // {
+        //     if(p != nullptr)
+        //     {
+        //         cout << p->data << " ";
+        //         preOrderRev(p->left);
+        //         preOrderRev(p->right);
+        //     }
+        // }
+
+        void preOrder()
+        {
+            
+        }
+
+        void levelOrder()
+        {
+            Node *p = root;
+            queue<Node *> que;
+            que.push(root);
+            while(!que.empty())
+            {
+                Node *q = que.front();
+                que.pop();
+                cout << q->data << " ";
+                if(q->left)
+                    que.push(q->left);
+                
+                if(q->right)
+                    que.push(q->right);
+            }
+        }
+
+        void postOrder()
+        {
+            
+        }
+
+};
+
 
 int main()
 {
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    Tree tree(arr, 10);
+
+    tree.levelOrder();
+
     return 0;
 }
