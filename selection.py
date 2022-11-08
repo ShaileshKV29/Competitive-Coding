8331e8f0c1ec72a96b202bd2a2a32c64268dafb1
ShaileshKV29 <shailesh29062001@gmail.com>
2022-11-09 00:00:09 +0530

    Selection Sort

---
+++ selection.py
@@ -0,0 +1,10 @@
+arr = input("").split()
+for i in range(len(arr)):
+    arr[i] = int(arr[i])
+
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if(arr[i] > arr[j]):
+            arr[i], arr[j] = arr[j], arr[i]
+
+print(arr)
